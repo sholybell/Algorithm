fad1eb690b6fec565114f129c96403d618a88eaf
holybell <holybell@vip.qq.com>
2020-06-01 23:02:36 +0800

    init

---
+++ src/main/java/com/holybell/algorithm/util/Util.java
@@ -0,0 +1,10 @@
+package com.holybell.algorithm.util;
+
+public class Util {
+
+    public static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
