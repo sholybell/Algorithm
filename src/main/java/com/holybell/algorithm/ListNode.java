56372e5e6c4f10f0f3d16115fa490e3dcc3629cf
holybell <holybell@vip.qq.com>
2020-06-03 09:34:49 +0800

    新增计算滑动窗口最大值算法

---
+++ src/main/java/com/holybell/algorithm/ListNode.java
@@ -0,0 +1,11 @@
+package com.holybell.algorithm;
+
+public class ListNode {
+
+    public int data;
+    public ListNode next;
+
+    public ListNode(int data) {
+        this.data = data;
+    }
+}
