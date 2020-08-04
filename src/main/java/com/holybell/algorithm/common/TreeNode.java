822a23d3ca4ab58be359aab3e8901a4a4840297c
holybell <holybell@vip.qq.com>
2020-08-04 22:06:33 +0800

    中等|新增计算二叉树左右子树最大深度和|--

--- src/main/java/com/holybell/algorithm/common/TreeNode.java
+++ src/main/java/com/holybell/algorithm/common/TreeNode.java
@@ -6,6 +6,8 @@ public class TreeNode {
 
     public TreeNode left;
     public TreeNode right;
+    public int leftMaxDistance; // 左子树距离根节点的最大距离
+    public int rightMaxDistance; // 右子树距离根节点的最大距离
 
     public int getData() {
         return data;
@@ -29,6 +31,22 @@ public class TreeNode {
 
     public void setRight(TreeNode right) {
         this.right = right;
+    }
+
+    public int getLeftMaxDistance() {
+        return leftMaxDistance;
+    }
+
+    public void setLeftMaxDistance(int leftMaxDistance) {
+        this.leftMaxDistance = leftMaxDistance;
+    }
+
+    public int getRightMaxDistance() {
+        return rightMaxDistance;
+    }
+
+    public void setRightMaxDistance(int rightMaxDistance) {
+        this.rightMaxDistance = rightMaxDistance;
     }
 
     public TreeNode() {
