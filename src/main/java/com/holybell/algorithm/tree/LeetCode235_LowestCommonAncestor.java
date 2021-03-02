ab68ebc46d2b620cf199c98f603d189dd0dce3c9
holybell <holybell@vip.qq.com>
2021-03-02 17:33:57 +0800

    简单|查找一颗二叉搜索树中指定p、q节点的最近祖先节点|新增

--- src/main/java/com/holybell/algorithm/tree/LeetCode235_LowestCommonAncestor.java
+++ src/main/java/com/holybell/algorithm/tree/LeetCode235_LowestCommonAncestor.java
@@ -2,19 +2,39 @@ package com.holybell.algorithm.tree;
 
 import com.holybell.algorithm.common.TreeNode;
 
+import java.util.*;
+
 /**
  * 难度:简单
- * <p>
  * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
  * <p>
- * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x
- * 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，
+ * 最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
  * <p>
- * 其实就是求最后一个让他们分叉的节点，分叉节点的父节点也是祖先节点
+ * 例如，给定如下二叉搜索树: root = [6,2,8,0,4,7,9,null,null,3,5]
  * <p>
- * https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * <p>
+ * 示例 1:
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * 输出: 6
+ * 解释: 节点 2 和节点 8 的最近公共祖先是 6。
+ * <p>
+ * <p>
+ * 示例 2:
+ * 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * 输出: 2
+ * 解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+ * <p>
+ * <p>
+ * 说明:
+ * <p>
+ * <p>
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉搜索树中。
+ * <p>
+ * Related Topics 树
  */
-public class Question051_LowestCommonAncestor {
+public class LeetCode235_LowestCommonAncestor {
 
 
     /**
@@ -26,7 +46,17 @@ public class Question051_LowestCommonAncestor {
      */
     private static TreeNode myLowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 
-        return new TreeNode();
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val < p.val && root.val < q.val) {
+            return myLowestCommonAncestor(root.right, p, q);
+        } else if (root.val > p.val && root.val > q.val) {
+            return myLowestCommonAncestor(root.left, p, q);
+        } else {
+            return root;
+        }
     }
 
     // --------------------------------------------------------------------
@@ -64,6 +94,40 @@ public class Question051_LowestCommonAncestor {
     // --------------------------------------------------------------------
     // --------------------------------------------------------------------
 
+    private static TreeNode lowestCommonAncestorV1(TreeNode root, TreeNode p, TreeNode q) {
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        // 记录每一个节点的父节点信息
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.left != null) {
+                parentMap.put(node.left, node);
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                parentMap.put(node.right, node);
+                queue.offer(node.right);
+            }
+        }
+
+        // 遍历其中一个节点p到达根节点的所有路径（包含自己）
+        Set<TreeNode> ancestor = new HashSet<>();
+        while (p != null) {
+            ancestor.add(p);
+            p = parentMap.get(p);
+        }
+
+        // 遍历q节点到根节点的路径，判断路径上的每个节点第一个出现在p的路径集合中的节点
+        while (!ancestor.contains(q)) {
+            q = parentMap.get(q);
+        }
+
+        return q;
+    }
+
     /**
      * 查找二叉树中节点p和节点q的分支节点
      * <p>
@@ -71,24 +135,21 @@ public class Question051_LowestCommonAncestor {
      *
      * @param root 当前要判断是否为分支节点的节点
      */
-    private static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    private static TreeNode lowestCommonAncestorV2(TreeNode root, TreeNode p, TreeNode q) {
 
-        // 当前节点值
-        int curVal = root.val;
-        // 节点p的值
-        int pVal = p.val;
-        // 节点q的值
-        int qVal = q.val;
+        if (root == null) {
+            return null;
+        }
 
-
-        if (pVal > curVal && qVal > curVal) {   // 节点p和节点q的值都比当前节点大
-            // 递归到右子节点判断
-            return lowestCommonAncestor(root.right, p, q);
-        } else if (pVal < curVal && qVal < curVal) {    // 节点p和节点q的值都比当前节点小
-            // 递归到左子节点判断
-            return lowestCommonAncestor(root.left, p, q);
+        // 利用二叉搜索树特性 如果当前节点的值同时小于p和q的值，那么p、q的共同祖先应该从root的右子节点继续寻找
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestorV2(root.right, p, q);
+        }
+        // 利用二叉搜索树特性 如果当前节点的值同时大于p和q的值，那么p、q的共同祖先应该从root的左子节点继续寻找
+        else if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestorV2(root.left, p, q);
         } else {
-            // p\q两个值刚好将当前节点夹在中间，那么就是当前节点
+            // 当前节点的值 刚好位于p、q节点之间(或者为其中一个节点)，划分了p和q，即为结果
             return root;
         }
     }
@@ -113,12 +174,10 @@ public class Question051_LowestCommonAncestor {
         node8.left = node7;
         node8.right = node9;
 
-        System.out.println("正确答案:" + lowestCommonAncestor(node6, node2, node4).val);
-        System.out.println("正确答案:" + lowestCommonAncestor(node6, node2, node8).val);
-
-        System.out.println("--------------->");
-
         System.out.println("本次答案:" + myLowestCommonAncestor(node6, node2, node4).val);
         System.out.println("本次答案:" + myLowestCommonAncestor(node6, node2, node8).val);
+        System.out.println("--------------->");
+        System.out.println("正确答案:" + lowestCommonAncestorV1(node6, node2, node4).val);
+        System.out.println("正确答案:" + lowestCommonAncestorV2(node6, node2, node8).val);
     }
 }
