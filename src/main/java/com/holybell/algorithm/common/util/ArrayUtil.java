75e414d18dd3fe36225bd74db7194b5fb1494f45
holybell <holybell@vip.qq.com>
2021-03-07 21:57:28 +0800

    困难|解数独|修改

---
+++ src/main/java/com/holybell/algorithm/common/util/ArrayUtil.java
@@ -0,0 +1,13 @@
+package com.holybell.algorithm.common.util;
+
+import java.util.Arrays;
+
+public abstract class ArrayUtil {
+
+    public static void printBoard(char[][] board) {
+        StringBuilder sb = new StringBuilder();
+        for (char[] row : board) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
