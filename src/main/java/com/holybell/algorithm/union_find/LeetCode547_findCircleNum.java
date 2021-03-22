920e6e083375d3733b54e4f586e47ffddd3f60b2
holybell <holybell@vip.qq.com>
2021-03-22 10:17:10 +0800

    中等|省份数量|修改

--- src/main/java/com/holybell/algorithm/union_find/LeetCode547_findCircleNum.java
+++ src/main/java/com/holybell/algorithm/union_find/LeetCode547_findCircleNum.java
@@ -1,7 +1,6 @@
-package com.holybell.algorithm.board.dfs;
+package com.holybell.algorithm.union_find;
 
-import java.util.HashSet;
-import java.util.Set;
+import com.holybell.algorithm.common.util.ArrayUtil;
 
 /**
  * 难度:中等
@@ -34,6 +33,8 @@ import java.util.Set;
  * isConnected[i][j] 为 1 或 0
  * isConnected[i][i] == 1
  * isConnected[i][j] == isConnected[j][i]
+ * <p>
+ * Related Topics 深度优先搜索 并查集
  */
 public class LeetCode547_findCircleNum {
 
@@ -83,7 +84,7 @@ public class LeetCode547_findCircleNum {
     /**
      * 这一定是一个正方形矩阵，长宽就是城市数量
      */
-    private static int findCircleNum(int[][] isConnected) {
+    private static int findCircleNumV1(int[][] isConnected) {
         int m = isConnected.length;             // 所有城市数量
         boolean[] visited = new boolean[m];     // 记录每个已经被DFS过的城市
         int provinces = 0;
@@ -103,17 +104,103 @@ public class LeetCode547_findCircleNum {
                 dfs(isConnected, visited, citys, j);
             }
         }
+    }
 
+    // --------------------------------------------------------------------
+
+    /**
+     * 并查集解法
+     *
+     * 与{@link LeetCode200_numIslands} 比对，本题的点位有实际意义，横竖轴每一个点位都表示同一个城市，无须上下左右4个方向扩散遍历
+     */
+    private static int findCircleNumV2(int[][] isConnected) {
+        int citys = isConnected.length;
+
+        UnionFind uf = new UnionFind(isConnected);
+
+        for (int i = 0; i < citys; i++) {
+            for (int j = 0; j < citys; j++) {
+                if (isConnected[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        return uf.getCount();
+    }
+
+    static class UnionFind {
+
+        private int[] roots;
+        private int count = 0;
+
+        /**
+         * 与{@link LeetCode200_numIslands} 比对
+         */
+        public UnionFind(int[][] isConnected) {
+            int citys = isConnected.length;
+            roots = new int[citys];
+            for (int i = 0; i < citys; i++) {
+                roots[i] = i;
+                count++;
+            }
+
+        }
+
+        public void union(int x, int y) {
+            int xRoot = find(x);
+            int yRoot = find(y);
+
+            if (xRoot != yRoot) {
+                roots[xRoot] = yRoot;
+                count--;
+            }
+        }
+
+        private int find(int x) {
+            int root = x;
+            while (roots[root] != root) {
+                root = roots[root];
+            }
+
+            while (root != x) {
+                int tmp = roots[x];
+                roots[x] = root;
+                x = tmp;
+            }
+            return root;
+        }
+
+        public int getCount() {
+            return count;
+        }
     }
 
     public static void main(String[] args) {
-        int[][] grid = new int[][]{
+        int[][] grid1 = new int[][]{
                 {1, 1, 0},
                 {1, 1, 0},
                 {0, 0, 1}
         };
-        System.out.println("你的答案:" + myFindCircleNum(grid));
+
+        int[][] grid2 = new int[][]{
+                {1, 0, 0, 1},
+                {0, 1, 1, 0},
+                {0, 1, 1, 1},
+                {1, 0, 1, 1}
+        };
+
+        System.out.println("二维网格1:");
+        ArrayUtil.printBoard(grid1);
+        System.out.println("二维网格2:");
+        ArrayUtil.printBoard(grid2);
+
+
+        System.out.println("你的答案:");
+        System.out.println("二维网格1省份 : " + myFindCircleNum(grid1));
+        System.out.println("二维网格2省份 : " + myFindCircleNum(grid2));
         System.out.println("----------------------------->");
-        System.out.println("正确答案:" + findCircleNum(grid));
+        System.out.println("正确答案:");
+        System.out.println("二维网格1省份 : " + findCircleNumV1(grid1));
+        System.out.println("二维网格2省份 : " + findCircleNumV2(grid2));
     }
 }
